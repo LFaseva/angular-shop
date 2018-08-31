3d00e5e8ba35d5d4e920b09883996d34ebcfd1a2
Elena Tverdohleb <tverdohlebe@VRP.local>
2018-08-31 10:02:13 +0300

    load product information with picture on DB with all discription fields

--- backEnd/routes/apiGridFs.js
+++ backEnd/routes/apiGridFs.js
@@ -9,41 +9,32 @@ const Grid = require('gridfs-stream');
 const methodOverride = require('method-override');
 const URI = 'mongodb://admin:Lenuska110589@ds125602.mlab.com:25602/shop';
 const schemaProduct = '../models/Product';
-const conn = mongoose.createConnection(URI);
+const conn = mongoose.createConnection(URI, { useNewUrlParser: true });
 
 // init gfs
 let gfs;
+
 //create stream
 conn.once('open', () => {
     gfs = new Grid(conn.db, mongoose.mongo);
-    gfs.collection('products');
 })
 
 //create storage
 const storage = new GridFsStorage({
     url: URI,
     file: (req, file) => {
-        console.log('file',file);
-        console.log('file',req.body);
-        return new Promise((resolve, reject) => {
-            crypto.randomBytes(16, (err, buf) => {
-                if (err) {
-                    return reject(err);
-                }
-                const filename = buf.toString('hex') + path.extname(file.originalname);
-                const fileInfo = {
-                    filename: filename,
-                    // bucketName: 'products'
-                };
-                resolve(fileInfo);
-            });
-        });
+        return {
+            filename: file.originalname + '_' + Date.now(),
+            metadata: req.body,
+            bucketName: 'products'
+        }
     }
 });
 
 const upload = multer({ storage }).any();
 
 app.post('/upload', upload, (req, res) => {
+    res.json({ success: true, msg: 'save file to DB successful'});
 })
 
 
